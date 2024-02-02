50d9f80f16b9b286490cea357658bcf69450d7e9
DeadMakar <ssapientiaa@gmail.com>
2024-02-02 17:28:02 +0200

    hw-04

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -1,7 +1,15 @@
-const Contact = require("../../models");
+const { Contact } = require("../../models");
 
 const getAllContacts = async (req, res, next) => {
-  const allContacts = await Contact.find();
+  const { _id: owner } = req.user;
+
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const allContacts = await Contact.find({ owner }, "-updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "email");
+
   res.status(200).json(allContacts);
 };
 
--- controllers/contacts/updateContact.js
+++ controllers/contacts/updateContact.js
@@ -1,11 +1,17 @@
 const { HttpError } = require("../../helpers");
-const Contact = require("../../models");
+const { Contact } = require("../../models");
 
 const updateContact = async (req, res) => {
   const { id } = req.params;
-  const changeContact = await Contact.findByIdAndUpdate(id, req.body, {
-    new: true,
-  });
+  const { _id } = req.user;
+  const changeContact = await Contact.findOneAndUpdate(
+    {
+      _id: id,
+      owner: _id,
+    },
+    req.body,
+    { new: true }
+  );
   if (!changeContact) {
     throw HttpError(404);
   }
